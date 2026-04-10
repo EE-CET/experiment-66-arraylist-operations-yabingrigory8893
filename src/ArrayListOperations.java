eb7ccd24ff84a97874e38c73bc87ca7b1ac66976
yabingrigory8893 <yabingrigory3@gmail.com>
2026-04-10 07:09:55 -0700

    Update ArrayListOperations.java

--- src/ArrayListOperations.java
+++ src/ArrayListOperations.java
@@ -2,9 +2,33 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class ArrayListOperations {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+           int n = Integer.parseInt(scanner.nextLine());
+        String[] items = scanner.nextLine().split(" ");
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(items[i]);
+        }
+
+        // Sort list
+        Collections.sort(list);
+
+        // Search item
+        String searchItem = scanner.nextLine();
+
+        // Output
+        System.out.println("Sorted Items: " + list);
+
+        if (list.contains(searchItem)) {
+            System.out.print("Found");
+        } else {
+            System.out.print("Not Found");
+        }
     }
 }
+    
+
